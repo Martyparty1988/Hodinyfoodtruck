cb7080c15ea9ff92da636d5ace7cfb0917be5962
Marty <rosary-vassals8m@icloud.com>
2025-08-25 09:17:10 +0200

    Create charts.js

---
+++ modules/charts.js
@@ -0,0 +1,15 @@
+// modules/charts.js - Canvas 2D grafy a heatmapa
+
+export function drawTimeChart(canvas, data) {
+    const ctx = canvas.getContext('2d');
+    // TODO: Draw line chart for time
+}
+
+export function drawEarningsChart(canvas, data) {
+    // TODO: Similar
+}
+
+export function drawProductivityHeatmap(canvas, data) {
+    const ctx = canvas.getContext('2d');
+    // TODO: Grid with colors based on productivity
+}
